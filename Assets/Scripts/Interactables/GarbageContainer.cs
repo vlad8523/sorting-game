a83ec29327d8d2420d05fa2be0e81a05abfa04de
Svinarenko Vladislav <vladushek12@gmail.com>
2023-12-10 16:40:32 +0300

    Добавлена система подсчета очков

--- Assets/Scripts/Interactables/GarbageContainer.cs
+++ Assets/Scripts/Interactables/GarbageContainer.cs
@@ -1,14 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Interactables
 {
     public class GarbageContainer : MonoBehaviour
     {
+        public GameObject scoreGameObject;
+        
         public InteractableType type;
+
+        public static event Action<int> OnUpdateScore;
+
+        private int _score = 100;
 
         public void Interact(InteractiveScript interactive)
         {
             Debug.Log(type == interactive.type);
+
+            if (type == interactive.type)
+            {
+                OnUpdateScore?.Invoke(_score);
+            }
+            else
+            {
+                OnUpdateScore?.Invoke(-_score);
+            }
         }
     }
 }
---
+++ Assets/Scripts/ScoreContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Interactables;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ScoreContainer : MonoBehaviour
+    {
+        private static int _score;
+
+        private void OnDisable()
+        {
+            GarbageContainer.OnUpdateScore -= GarbageContainer_onUpdateScore;
+        }
+        
+        private void OnEnable()
+        {
+            GarbageContainer.OnUpdateScore += GarbageContainer_onUpdateScore;
+        }
+
+        private void GarbageContainer_onUpdateScore(int value)
+        {
+            _score += value;
+            Debug.Log($"current score: {_score}");
+        }
+    }
+}
